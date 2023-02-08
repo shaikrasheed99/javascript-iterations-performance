e18a435c211bbadc44deb7d67f75e9a02dda35f8
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-02-08 23:38:56 +0530

    Add. For of loop test

--- test.js
+++ test.js
@@ -3,3 +3,9 @@ const millionTimes = 1_000_000;
 console.time('Traditional for loop time: ');
 for (let index = 0; index < millionTimes; index++) {}
 console.timeEnd('Traditional for loop time: ');
+
+const arrayWithMillionZeros = Array(millionTimes).fill(0);
+
+console.time('For of time: ');
+for (let value of arrayWithMillionZeros) {}
+console.timeEnd('For of time: ');

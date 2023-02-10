c2752c66b9b74e52224800718ea4484b5d809841
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-02-10 17:14:08 +0530

    Refactor. Whole test file

--- test.js
+++ test.js
@@ -1,29 +1,29 @@
 const millionTimes = 1_000_000;
 
-console.time('Traditional while loop time: ');
+console.time('Traditional while loop time');
 let iterator = 0;
 while (iterator < millionTimes) {
     iterator++;
 }
-console.timeEnd('Traditional while loop time: ');
+console.timeEnd('Traditional while loop time');
 
-console.time('Traditional do-while loop time: ');
+console.time('Traditional do-while loop time');
 let index = 0;
 do {
     index++;
 } while (index < millionTimes);
-console.timeEnd('Traditional do-while loop time: ');
+console.timeEnd('Traditional do-while loop time');
 
-console.time('Traditional for loop time: ');
+console.time('Traditional for loop time');
 for (let index = 0; index < millionTimes; index++) {}
-console.timeEnd('Traditional for loop time: ');
+console.timeEnd('Traditional for loop time');
 
 const arrayWithMillionZeros = Array(millionTimes).fill(0);
 
-console.time('For of loop time: ');
+console.time('For of loop time');
 for (let value of arrayWithMillionZeros) {}
-console.timeEnd('For of loop time: ');
+console.timeEnd('For of loop time');
 
-console.time('For in loop time: ');
+console.time('For in loop time');
 for (let value in arrayWithMillionZeros) {}
-console.timeEnd('For in loop time: ');
+console.timeEnd('For in loop time');

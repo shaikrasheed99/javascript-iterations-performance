16efa7b7b3fa5ec3c7925f6ec15b261d6d8a848c
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-02-10 17:11:01 +0530

    Add. Traditional while loop test

--- test.js
+++ test.js
@@ -1,5 +1,12 @@
 const millionTimes = 1_000_000;
 
+console.time('Traditional while loop time: ');
+let iterator = 0;
+while (iterator < millionTimes) {
+    iterator++;
+}
+console.timeEnd('Traditional while loop time: ');
+
 console.time('Traditional for loop time: ');
 for (let index = 0; index < millionTimes; index++) {}
 console.timeEnd('Traditional for loop time: ');
